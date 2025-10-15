fae46191581b0237d98840d4e1892ca89472451e
nguyendan1337 <nguyendan1337@yahoo.com>
2025-10-15 15:18:18 -0700

    test joke api

--- pythonTest/test.py
+++ pythonTest/test.py
@@ -1,1 +1,9 @@
+import requests
+
 print("hellp")
+response = requests.get("https://official-joke-api.appspot.com/random_joke")
+if response.status_code == 200:
+    joke = response.json()
+    print(f"{joke['setup']} {joke['punchline']}")
+else:
+    print("Failed to retrieve a joke.")
